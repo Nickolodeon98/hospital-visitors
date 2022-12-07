09586bfe9852e1eaa60d3c0aea47e3b1938a4cc6
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 17:30:24 +0900

    feat: Hospital 과 Disease 테이블에 항목 추가를 위해 Hospital 엔티티에 우선 컬럼명들 추가

--- src/main/java/com/example/hospitalvisitors/domain/entity/Hospital.java
+++ src/main/java/com/example/hospitalvisitors/domain/entity/Hospital.java
@@ -3,6 +3,7 @@ package com.example.hospitalvisitors.domain.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -20,6 +21,18 @@ public class Hospital {
     private String hospitalName;
     private String roadNameAddress;
     private String businessTypeName;
+    private String openServiceName;
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
 
     @OneToMany(mappedBy = "hospital")
     private List<Visit> visits;
