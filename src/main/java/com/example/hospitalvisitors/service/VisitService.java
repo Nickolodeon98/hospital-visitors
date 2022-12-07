7eb76bf9298c1fe897f255723193282999107940
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 14:39:36 +0900

    feat: VisitService 에서 주어진 정보를 가지고 Visit 테이블에 row 를 추가하는 로직 구현

--- src/main/java/com/example/hospitalvisitors/service/VisitService.java
+++ src/main/java/com/example/hospitalvisitors/service/VisitService.java
@@ -2,6 +2,10 @@ package com.example.hospitalvisitors.service;
 
 import com.example.hospitalvisitors.domain.dto.VisitRequest;
 import com.example.hospitalvisitors.domain.dto.VisitResponse;
+import com.example.hospitalvisitors.domain.entity.User;
+import com.example.hospitalvisitors.domain.entity.Visit;
+import com.example.hospitalvisitors.repository.UserRepository;
+import com.example.hospitalvisitors.repository.VisitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +14,13 @@ import org.springframework.stereotype.Component;
 public class VisitService {
 
     private final VisitRepository visitRepository;
+    private final UserRepository userRepository;
 
-    public VisitResponse writeNewRecord(String uid, VisitRequest visitRequest) {
-
+    public VisitResponse writeNewRecord(String uid, VisitRequest visitRequest) { // specific user id is conveyed
+        Visit visit = visitRequest.toEntity(); // user id is then saved as well as any other info in visitRequest
+        User user = userRepository.findByUid(uid).orElseThrow(RuntimeException::new);
+        visit.setUserId(user);
+        Visit savedVisit = visitRepository.save(visit);
+        return Visit.of(savedVisit);
     }
 }
