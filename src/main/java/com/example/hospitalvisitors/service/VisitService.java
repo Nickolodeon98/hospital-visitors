833c3a89a265f4bf0237a7fd166bc88e518ab0f1
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 14:04:28 +0900

    feat: 서비스에서 리포지토리 DI

--- src/main/java/com/example/hospitalvisitors/service/VisitService.java
+++ src/main/java/com/example/hospitalvisitors/service/VisitService.java
@@ -1,8 +1,17 @@
 package com.example.hospitalvisitors.service;
 
+import com.example.hospitalvisitors.domain.dto.VisitRequest;
+import com.example.hospitalvisitors.domain.dto.VisitResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class VisitService {
 
+    private final VisitRepository visitRepository;
+
+    public VisitResponse writeNewRecord(String uid, VisitRequest visitRequest) {
+
+    }
 }
