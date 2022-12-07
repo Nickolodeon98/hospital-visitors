84d8ace82c194b1b32b47bb9345016d97c900295
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 17:14:56 +0900

    feat: VisitService 에서 API 요청으로 병원 명과 병명을 받아서 Visit 테이블에 저장하는 로직 구현

--- src/main/java/com/example/hospitalvisitors/service/VisitService.java
+++ src/main/java/com/example/hospitalvisitors/service/VisitService.java
@@ -2,6 +2,8 @@ package com.example.hospitalvisitors.service;
 
 import com.example.hospitalvisitors.domain.dto.VisitRequest;
 import com.example.hospitalvisitors.domain.dto.VisitResponse;
+import com.example.hospitalvisitors.domain.entity.Disease;
+import com.example.hospitalvisitors.domain.entity.Hospital;
 import com.example.hospitalvisitors.domain.entity.User;
 import com.example.hospitalvisitors.domain.entity.Visit;
 import com.example.hospitalvisitors.repository.DiseaseRepository;
@@ -27,11 +29,13 @@ public class VisitService {
         User user = userRepository.findByUid(uid).orElseThrow(RuntimeException::new);
         visit.setUser(user); // 찾은 User 객체를 visit 테이블에 추가한다.
 
-        /* 위에서 사용자 아이디로 찾았듯이, 각각 병명과 병원 명으로 Disease 와 Hospital 객체를 찾는다. */
+        /* 위에서 사용자 아이디로 찾았듯이, 각각 병명과 병원 명으로 Disease 와 Hospital 객체를 찾아서 저장한다. */
+        Disease disease = diseaseRepository.findByDiseaseName(visitRequest.getDiseaseName()).orElseThrow(RuntimeException::new);
+        visit.setDisease(disease); // 찾은 Disease 를 visit 테이블에 추가한다.
 
+        Hospital hospital = hospitalRepository.findByHospitalName(visitRequest.getHospitalName()).orElseThrow(RuntimeException::new);
+        visit.setHospital(hospital); // 찾은 Hospital 를 visit 테이블에 추가한다.
 
-        visit.setDisease(); // 찾은 Disease 를 visit 테이블에 추가한다.
-        visit.setHospital() // 찾은 Hospital 를 visit 테이블에 추가한다.
         Visit savedVisit = visitRepository.save(visit);
         return Visit.of(savedVisit);
     }
