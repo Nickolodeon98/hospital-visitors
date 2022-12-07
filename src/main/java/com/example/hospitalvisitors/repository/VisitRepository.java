48e10c70466933a69b3edbc52720ebf60535f7f1
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 15:00:04 +0900

    feat: Visit 리포지토리에서 Visit 엔티티 import

--- src/main/java/com/example/hospitalvisitors/repository/VisitRepository.java
+++ src/main/java/com/example/hospitalvisitors/repository/VisitRepository.java
@@ -1,5 +1,6 @@
 package com.example.hospitalvisitors.repository;
 
+import com.example.hospitalvisitors.domain.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VisitRepository extends JpaRepository<Visit, Long> {
