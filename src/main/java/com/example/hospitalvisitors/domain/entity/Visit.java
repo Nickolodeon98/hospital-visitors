0c243ad8c18331127e253d8efc15adf07ba815bb
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 14:11:15 +0900

    feat: Visit 엔티티에 들어갈 컬럼 내용들 변수로 작성

--- src/main/java/com/example/hospitalvisitors/domain/entity/Visit.java
+++ src/main/java/com/example/hospitalvisitors/domain/entity/Visit.java
@@ -1,4 +1,37 @@
 package com.example.hospitalvisitors.domain.entity;
 
+import com.fasterxml.jackson.databind.DatabindException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Visit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 방문 내역 각각의 고유 아이디
+
+    private Long diagnosisFee;
+    private Date recordedAt;
+
+    @OneToOne
+    private Disease disease;
+
+    @ManyToOne
+    private Hospital hospitalId;
+
+    @ManyToOne
+    private User userId;
+
+
+
 }
