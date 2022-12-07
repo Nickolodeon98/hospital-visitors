c66487989b93e8d8e5acc518fda0b3649ecca6ac
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 14:05:51 +0900

    feat: VisitResponse 와 VisitRequest dto 멤버 변수와 빌더 추가

--- src/main/java/com/example/hospitalvisitors/domain/dto/VisitRequest.java
+++ src/main/java/com/example/hospitalvisitors/domain/dto/VisitRequest.java
@@ -1,9 +1,14 @@
 package com.example.hospitalvisitors.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class VisitRequest {
+
+    private Long hospitalId;
+    private Long diseaseId;
 }
--- src/main/java/com/example/hospitalvisitors/domain/dto/VisitResponse.java
+++ src/main/java/com/example/hospitalvisitors/domain/dto/VisitResponse.java
@@ -1,9 +1,15 @@
 package com.example.hospitalvisitors.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class VisitResponse {
+
+    private String userName;
+    private String hospitalName;
+    private String diseaseName;
 }
