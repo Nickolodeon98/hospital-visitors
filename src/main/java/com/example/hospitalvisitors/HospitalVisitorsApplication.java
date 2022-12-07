1c3f8253a24b88d158ba50bb2b5bd5efe42b2bd3
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 11:44:08 +0900

    feat: 사용자가 병원 방문 내용을 기록할 수 있는 새 프로젝트 시작

---
+++ src/main/java/com/example/hospitalvisitors/HospitalVisitorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalvisitors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalVisitorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalVisitorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hospitalvisitors/HospitalVisitorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalvisitors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalVisitorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
