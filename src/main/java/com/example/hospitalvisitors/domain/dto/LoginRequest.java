8963e3213083b0888843dad8863072d64a57b82f
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 16:34:06 +0900

    feat: 로그인 요청 시 User 객체를 저장하기 위해 LoginRequest DTO 에 User 엔티티화하는 toEntity() 메서드 추가

--- src/main/java/com/example/hospitalvisitors/domain/dto/LoginRequest.java
+++ src/main/java/com/example/hospitalvisitors/domain/dto/LoginRequest.java
@@ -1,11 +1,23 @@
 package com.example.hospitalvisitors.domain.dto;
 
+import com.example.hospitalvisitors.domain.entity.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
+@Setter
 public class LoginRequest {
 
     private String userId;
+
+    public User toEntity() {
+        return User.builder()
+                .uid(userId)
+                .build();
+    }
 }
