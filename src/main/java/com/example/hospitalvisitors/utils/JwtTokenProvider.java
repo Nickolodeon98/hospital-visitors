0671232a3e844e3c85a289a6b399b8a135226a7b
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 15:43:36 +0900

    feat: JwtTokenProvider 에 토큰 유효성 검사, 토큰으로부터 정보 추출, 인증된 토큰 생성하는 메서드 구현

--- src/main/java/com/example/hospitalvisitors/utils/JwtTokenProvider.java
+++ src/main/java/com/example/hospitalvisitors/utils/JwtTokenProvider.java
@@ -4,9 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
@@ -23,5 +27,21 @@ public class JwtTokenProvider {
                 .setExpiration(new Date(now.getTime() + (1000L * 60 * 60)))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+    }
+
+    public static Claims extractClaims(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        return extractClaims(token, secretKey).getExpiration().before(new Date()); // 만료일이 오늘보다 이전이면 만료되었다.
+    }
+
+    public static String getUserId(String token, String secretKey) {
+        return extractClaims(token, secretKey).getSubject();
+    }
+
+    public static Authentication getAuthentication(String userId) {
+        return new UsernamePasswordAuthenticationToken(userId, "", List.of(new SimpleGrantedAuthority("USER")));
     }
 }
