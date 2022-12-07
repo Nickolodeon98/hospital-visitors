69f260482883d9a7fad807277bb1c68c4c4a9723
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 14:05:05 +0900

    feat: API 가 서비스에서 VisitResponse dto 만들어서 반환하도록 작성

--- src/main/java/com/example/hospitalvisitors/controller/VisitController.java
+++ src/main/java/com/example/hospitalvisitors/controller/VisitController.java
@@ -5,6 +5,7 @@ import com.example.hospitalvisitors.domain.dto.VisitResponse;
 import com.example.hospitalvisitors.service.VisitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,9 @@ public class VisitController {
     /* 첫 번째 API:
      * TODO: visit 테이블에 사용자에 대한 정보와 방문한 병원 정보를 저장한다 */
     @PostMapping("/new")
-    public ResponseEntity<VisitResponse> recordHistory(@RequestBody VisitRequest visitRequest) {
-        return ResponseEntity.ok().body(new VisitResponse());
+    public ResponseEntity<VisitResponse> recordHistory(@RequestBody VisitRequest visitRequest, Authentication authentication) {
+        VisitResponse visitResponse = visitService.writeNewRecord(authentication.getName(), visitRequest);
+        return ResponseEntity.ok().body(visitResponse);
     }
 
 }
