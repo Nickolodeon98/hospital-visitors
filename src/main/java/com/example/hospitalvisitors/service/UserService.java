f9cf8c08d9d9d429054b4f2a6e1d42836b062803
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 15:28:53 +0900

    feat: UserService 에서 항상 같은 비밀 키로 토큰을 발급하도록 환경변수에서 비밀 키를 가져오도록 설정 한 뒤 토큰 생성 메서드 추가

--- src/main/java/com/example/hospitalvisitors/service/UserService.java
+++ src/main/java/com/example/hospitalvisitors/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.hospitalvisitors.service;
 
+import com.example.hospitalvisitors.repository.UserRepository;
+import com.example.hospitalvisitors.utils.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String authenticate(String uid) {
+        return JwtTokenProvider.createToken(secretKey, uid);
+    }
+
 }
