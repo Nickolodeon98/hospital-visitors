97c2451d4ed49ab91a64a0f59d76ec0ae5695b6a
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-07 15:28:17 +0900

    feat: Login 정보를 받는 LoginRequest dto 생성

---
+++ src/main/java/com/example/hospitalvisitors/domain/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.hospitalvisitors.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginRequest {
+
+    private String userId;
+}
