95e8f9eec227ab9759c28d215e34a5b361001832
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 01:03:56 +0900

    feat: 사용자를 DB에 저장하는 메서드 구현

--- src/main/java/com/example/hospitalvisitors/service/UserService.java
+++ src/main/java/com/example/hospitalvisitors/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.hospitalvisitors.service;
 
 import com.example.hospitalvisitors.domain.dto.LoginRequest;
+import com.example.hospitalvisitors.domain.entity.User;
 import com.example.hospitalvisitors.repository.UserRepository;
 import com.example.hospitalvisitors.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,11 @@ public class UserService {
     public String authenticate(LoginRequest loginRequest) {
         userRepository.save(loginRequest.toEntity());
         return JwtTokenProvider.createToken(secretKey, loginRequest.getUserId());
+    }
+
+    public String testSave(LoginRequest loginRequest) {
+        userRepository.save(loginRequest.toEntity());
+        return "";
     }
 
 }
