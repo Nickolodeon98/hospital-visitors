b0fa3db9bba26238f5e4d75824f11a0ac6daa4fa
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 12:58:42 +0900

    feat: 예외 상황 별로 다른 에러 코드를 받을 수 있도록 커스텀 예외 클래스 변경

--- src/main/java/com/example/hospitalvisitors/exception/JoinException.java
+++ src/main/java/com/example/hospitalvisitors/exception/JoinException.java
@@ -1,5 +1,11 @@
 package com.example.hospitalvisitors.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class JoinException extends RuntimeException {
 
+    private ErrorCode errorCode;
 }
