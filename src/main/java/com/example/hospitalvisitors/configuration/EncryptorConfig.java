354c9ad13b35851af77a986d176e7ca8afa65146
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 13:27:24 +0900

    feat: 비밀번호 인코딩을 위해서 BCryptPasswordEncoder 를 빈으로 등록

---
+++ src/main/java/com/example/hospitalvisitors/configuration/EncryptorConfig.java
@@ -0,0 +1,15 @@
+package com.example.hospitalvisitors.configuration;
+
+import lombok.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncryptorConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+}
