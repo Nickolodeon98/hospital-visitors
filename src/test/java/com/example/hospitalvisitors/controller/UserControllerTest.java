0d791135f512b3cd3cf02d3cb7cee47d536f4eaf
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 12:45:33 +0900

    feat: 테스트 코드에 회원가입 실패 시 테스트를 추가

--- src/test/java/com/example/hospitalvisitors/controller/UserControllerTest.java
+++ src/test/java/com/example/hospitalvisitors/controller/UserControllerTest.java
@@ -3,9 +3,11 @@ package com.example.hospitalvisitors.controller;
 import com.example.hospitalvisitors.domain.dto.JoinRequest;
 import com.example.hospitalvisitors.domain.dto.JoinResponse;
 import com.example.hospitalvisitors.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.boot.jaxb.hbm.spi.JaxbHbmNativeQueryJoinReturnType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 
@@ -36,22 +40,28 @@ class UserControllerTest {
 
     private final ObjectMapper objectMapper;
 
-    @Test
-    @DisplayName("회원가입에 성공한다.")
-    void join_success() throws Exception {
+    private JoinRequest joinRequest;
+    private JoinResponse joinResponse;
 
-        JoinRequest joinRequest = JoinRequest.builder()
+    @BeforeEach
+    void setUp() {
+        joinRequest = JoinRequest.builder()
                 .name("전승환")
                 .userId("sjeon0730")
                 .password("12345678")
                 .build();
 
-        JoinResponse joinResponse = JoinResponse.builder()
-                        .userId("sjeon0730")
-                        .name("전승환")
-                        .message("회원가입에 성공했습니다.")
-                        .build();
+        joinResponse = JoinResponse.builder()
+                .userId("sjeon0730")
+                .name("전승환")
+                .message("회원가입에 성공했습니다.")
+                .build();
+    }
 
+
+    @Test
+    @DisplayName("회원가입에 성공한다.")
+    void join_success() throws Exception {
         given(userService.joinUser(joinRequest)).willReturn(joinResponse);
 
         String url = "/api/users/join";
@@ -64,6 +74,20 @@ class UserControllerTest {
                 .andDo(print());
 
         verify(userService).joinUser(joinRequest);
+    }
 
+    @Test
+    @DisplayName("회원가입에 실패한다.")
+    void join_fail() throws Exception {
+        given(userService.joinUser(any())).willThrow(new HospitalAppException(ErrorCode.DUPLICATE_ID.name()));
+
+        String url = "/api/users/join";
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(any())))
+                .andExpect(status().isConflict())
+                .andDo(print());
+
+        verify(userService).joinUser(any());
     }
 }
