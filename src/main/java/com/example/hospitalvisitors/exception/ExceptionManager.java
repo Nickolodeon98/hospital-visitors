c7361c73405ba717cd366c37950a29dddb81a541
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 13:28:32 +0900

    feat: 에러 발생 시 JSON 형태 데이터가 아닌 에러 뷰 페이지로 응답하도록 변경

--- src/main/java/com/example/hospitalvisitors/exception/ExceptionManager.java
+++ src/main/java/com/example/hospitalvisitors/exception/ExceptionManager.java
@@ -2,19 +2,27 @@ package com.example.hospitalvisitors.exception;
 
 import com.example.hospitalvisitors.domain.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class ExceptionManager {
 
-
+//    @ExceptionHandler(JoinException.class)
+//    @ResponseBody
+//    public ResponseEntity<?> joinExceptionHandler(JoinException e) {
+//        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+//                .body(Response.error(e.getErrorCode().getMessage()));
+//    }
 
     @ExceptionHandler(JoinException.class)
-    public ResponseEntity<?> joinExceptionHandler(JoinException e) {
-        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
-                .body(Response.error(e.getErrorCode().getMessage()));
+    public String joinExceptionHandler(JoinException e, Model model) {
+        model.addAttribute("errorCode", e.getErrorCode().name());
+        model.addAttribute("message", e.getErrorCode().getMessage());
+        return "hospitals/error";
     }
-
 
 }
