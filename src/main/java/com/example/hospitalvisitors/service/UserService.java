d385a0a9073a16b3301a35c5dfae2afa7ae3ab7f
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 12:45:49 +0900

    feat: 회원가입 하는 로직 구현

--- src/main/java/com/example/hospitalvisitors/service/UserService.java
+++ src/main/java/com/example/hospitalvisitors/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.hospitalvisitors.service;
 
+import com.example.hospitalvisitors.domain.dto.JoinRequest;
+import com.example.hospitalvisitors.domain.dto.JoinResponse;
 import com.example.hospitalvisitors.domain.dto.LoginRequest;
 import com.example.hospitalvisitors.domain.entity.User;
 import com.example.hospitalvisitors.repository.UserRepository;
@@ -7,6 +9,7 @@ import com.example.hospitalvisitors.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,14 +21,17 @@ public class UserService {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    public JoinResponse joinUser(JoinRequest joinRequest) {
+        String encodedPassword = joinRequest.getPassword(); // 패스워드를 해싱합니다.
+        User savedUser = userRepository.save(joinRequest.toEntity(encodedPassword));
+        return JoinResponse.fromEntity(savedUser);
+    }
+
     public String authenticate(LoginRequest loginRequest) {
         userRepository.save(loginRequest.toEntity());
         return JwtTokenProvider.createToken(secretKey, loginRequest.getUserId());
     }
 
-    public String testSave(LoginRequest loginRequest) {
-        userRepository.save(loginRequest.toEntity());
-        return "";
-    }
+
 
 }
