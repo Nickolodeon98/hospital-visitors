e47234061692da2c276d4ddfaa69fd131715f823
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-10 12:31:00 +0900

    feat: UserRestController 와 UserController 병합

--- src/main/java/com/example/hospitalvisitors/controller/UserController.java
+++ src/main/java/com/example/hospitalvisitors/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.example.hospitalvisitors.controller;
 
+import com.example.hospitalvisitors.domain.dto.JoinRequest;
 import com.example.hospitalvisitors.domain.dto.LoginRequest;
 import com.example.hospitalvisitors.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/users")
@@ -17,15 +17,25 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/home")
+    @GetMapping("/join")
     public String homeScreen() {
-        return "hospitals/home";
+        return "/hospitals/join";
     }
 
-    @PostMapping("/test")
-    public String testLogin(LoginRequest loginRequest) {
-        log.info("userID:{}", loginRequest.getUserId());
-        userService.testSave(loginRequest);
+    @PostMapping("/join")
+    public String join(JoinRequest joinRequest) {
+        log.info("userID:{}", joinRequest.getUserId());
+        userService.joinUser(joinRequest);
         return "redirect:/api/users/home";
+    }
+
+    /* 두 번째 API:
+     * TODO: 명시하는 아이디로 로그인한다. */
+    @ResponseBody
+    @PostMapping("/login")
+    public ResponseEntity<String> loginWithUserId(@RequestBody LoginRequest loginRequest) {
+        log.info("loginRequest:{}", loginRequest);
+        String token = userService.authenticate(loginRequest);
+        return ResponseEntity.ok().body(token);
     }
 }
